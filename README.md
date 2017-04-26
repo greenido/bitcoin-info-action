215a04c0a5dce7e0b872d99b8c75d4d75161659f
Ido Green <greenido@gmail.com>
2017-04-25 21:31:10 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,46 @@
+# Actions on Google Webhook example for bitcoin-info codelab using Node.js
+
+This webhook example for bitcoin-info codelab sets up everything you need to build your fulfillment
+business logic for your API.AI bitcoin agent. It could fetch the current price or the total amount of bitcoins for you.
+
+## Setup Instructions
+
+### Pre-requisites
+ 1. API.AI account: [https://api.ai](https://api.ai)
+ 2. Google Cloud project: [https://console.cloud.google.com/project](https://console.cloud.google.com/project)
+
+See the developer guide and release notes at [https://developers.google.com/actions/](https://developers.google.com/actions/) for more details.
+
+### Steps
+1. Create a new agent in API.AI [https://api.ai](https://api.ai). You can also take this repo as a zip and import it to API.AI
+2. Deploy this action to your preferred hosting environment
+ (we recommend [Google Cloud Functions](https://cloud.google.com/functions/docs/tutorials/http)).
+3. Set the "Fulfillment" webhook URL to the hosting URL.
+4. In any relevant intents (*price* and *total*), enable the Fulfillment for the response.
+5. Build out your agent and business logic by adding function handlers for API.AI actions.
+6. For each API.AI action, set a new key/value pair on the actionMap, reflecting
+ the action name and corresponding function handler on the actionMap in **index.js**.
+1. Make sure all domains are turned off.
+1. Enable Actions on Google in the Integrations.
+1. Provide an invocation name for the action.
+1. Authorize and preview the action in the [web simulator](https://developers.google.com/actions/tools/web-simulator).
+
+For more detailed information on deployment, see the [documentation](https://developers.google.com/actions/samples/).
+
+## References and How to report bugs
+* Actions on Google documentation: [https://developers.google.com/actions/](https://developers.google.com/actions/).
+* If you find any issues, please open a bug here on GitHub.
+* Questions are answered on [StackOverflow](https://stackoverflow.com/questions/tagged/actions-on-google).
+
+## How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md.
+
+## License
+See LICENSE.md.
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [https://g.co/actionsdev](https://g.co/actionsdev).
+
---
+++ index.js
@@ -0,0 +1,64 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+// We need the Assistant client for all the magic here
+const Assistant = require('actions-on-google').ApiAiAssistant;
+// To make our http request (a bit nicer)
+const request = require('request');
+
+// Some variables we will use in this example
+const ACTION_PRICE = 'price';
+const ACTION_TOTAL = 'total';
+const EXT_BITCOIN_API_URL = "https://blockchain.info";
+const EXT_PRICE = "/q/24hrprice";
+const EXT_TOTAL = "/q/totalbc";
+
+// [START YourAction]
+exports.bitcoinInfo = (req, res) => {
+    const assistant = new Assistant({ request: req, response: res });
+    console.log('bitcoinInfoAction Request headers: ' + JSON.stringify(req.headers));
+    console.log('bitcoinInfoAction Request body: ' + JSON.stringify(req.body));
+
+    // Fulfill price action business logic
+    function priceHandler(assistant) {
+        request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
+            // The fulfillment logic for returning the bitcoin current price
+            console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+            const msg = "Right now the price of a bitcoin is " + body + " USD";
+            assistant.tell(msg);
+        });
+    }
+
+    // Fulfill total bitcoin action 
+    function totalHandler(assistant) {
+        request(EXT_BITCOIN_API_URL + EXT_TOTAL, function(error, response, body) {
+            console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+            // The fulfillment logic for returning the amount of bitcoins in the world
+            const billionsBitcoins = body / 1000000000;
+            const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world.";
+            assistant.tell(msg);
+        });
+    }
+
+    // The Entry point to all our actions
+    const actionMap = new Map();
+    actionMap.set(ACTION_PRICE, priceHandler);
+    actionMap.set(ACTION_TOTAL, totalHandler);
+
+    assistant.handleRequest(actionMap);
+};
+// [END Action]
