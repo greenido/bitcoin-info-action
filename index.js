202837213b5312199ad77e07c9ca0f13617000e0
Ido Green <greenido@gmail.com>
2017-05-01 15:14:58 -0700

    Add without breaking the const variable

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ exports.bitcoinInfo = (req, res) => {
         request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
             // The fulfillment logic for returning the bitcoin current price
             console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-            const msg = "Right now the price of a bitcoin is " + body + " USD. ";
-            msg += "What else would you like to know?";
+            const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
             assistant.tell(msg);
         });
     }
@@ -50,8 +49,7 @@ exports.bitcoinInfo = (req, res) => {
             console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
             // The fulfillment logic for returning the amount of bitcoins in the world
             const billionsBitcoins = body / 1000000000;
-            const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. ";
-            msg += "What else would you like to know?";
+            const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
             assistant.tell(msg);
         });
     }
