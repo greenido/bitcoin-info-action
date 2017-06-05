a54028c2c9ce20edeed9b3b5be514f765910e771
Ido Green <idog@idog-macbookpro.roam.corp.google.com>
2017-06-05 10:30:32 -0700

    Add the interval between blocks

--- index.js
+++ index.js
@@ -20,17 +20,20 @@ const Assistant = require('actions-on-google').ApiAiAssistant;
 // To make our http request (a bit nicer)
 const request = require('request');
 
-// Some variables we will use in this example
+// the actions we are supporting (get them from api.ai)
 const ACTION_PRICE = 'price';
 const ACTION_TOTAL = 'total';
 const ACTION_BLOCK = 'block';
 const ACTION_MARKET = 'marketcap';
+const ACTION_INTERVAL = 'interval';
 
+// The end-points to our calls
 const EXT_BITCOIN_API_URL = "https://blockchain.info";
 const EXT_PRICE = "/q/24hrprice";
 const EXT_TOTAL = "/q/totalbc";
 const EXT_BLOCK_COUNT = "/q/getblockcount";
 const EXT_MARKET_CAP = "/q/marketcap";
+const EXT_INTERVAL = "/q/interval";
 
 // [START Bitcoin Info]
 exports.bitcoinInfo = (req, res) => {
@@ -40,44 +43,52 @@ exports.bitcoinInfo = (req, res) => {
 
   // Fulfill price action business logic
   function priceHandler (assistant) {
-  	request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
-  		// The fulfillment logic for returning the bitcoin current price
-  		console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-    	const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
-    	assistant.ask(msg);
-  	});
+    request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
+      // The fulfillment logic for returning the bitcoin current price
+      console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+      const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
+      assistant.ask(msg);
+    });
   }
 
   // Fulfill total bitcoin action 
   function totalHandler (assistant) {
     request(EXT_BITCOIN_API_URL + EXT_TOTAL, function(error, response, body) {
-  		console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-    	// The fulfillment logic for returning the amount of bitcoins in the world
-    	const billionsBitcoins = body / 1000000000;
-    	const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
-    	assistant.ask(msg);
-  	});
+      console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+      // The fulfillment logic for returning the amount of bitcoins in the world
+      const billionsBitcoins = body / 1000000000;
+      const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
+      assistant.ask(msg);
+    });
   }
   
   // Fulfill block count action
   function blockCountHandler (assistant) {
-  	request(EXT_BITCOIN_API_URL + EXT_BLOCK_COUNT, function(error, response, body) {
-  		console.log("blockCountHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-    	const msg = "Right now there are " + body + " blocks. What else would you like to know? the price?";
-    	assistant.ask(msg);
-  	});
+    request(EXT_BITCOIN_API_URL + EXT_BLOCK_COUNT, function(error, response, body) {
+      console.log("blockCountHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+      const msg = "Right now there are " + body + " blocks. What else would you like to know? the price?";
+      assistant.ask(msg);
+    });
   }
 
-// Fulfill block count action
+  // Fulfill market cap action
   function marketCaptHandler (assistant) {
-  	request(EXT_BITCOIN_API_URL + EXT_MARKET_CAP, function(error, response, body) {
-  		console.log("marketCaptHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-  		const marketCapB = Math.round((body / 1000000000) * 100) / 100
-    	const msg = "Right now market cap is " + marketCapB + " billions. What else would you like to know?";
-    	assistant.ask(msg);
-  	});
+    request(EXT_BITCOIN_API_URL + EXT_MARKET_CAP, function(error, response, body) {
+      console.log("marketCaptHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+      const marketCapB = Math.round((body / 1000000000) * 100) / 100
+      const msg = "Right now market cap is " + marketCapB + " billions. What else would you like to know?";
+      assistant.ask(msg);
+    });
   }
-
+  
+  // Fulfill interval action
+  function intervalHandler (assistant) {
+    request(EXT_BITCOIN_API_URL + EXT_INTERVAL, function(error, response, body) {
+      console.log("interval response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+      const msg = "Right now the interval between blocks is " + body + " seconds. What else would you like to know?";
+      assistant.ask(msg);
+    });
+  }
 
   // The Entry point to all our actions
   const actionMap = new Map();
@@ -85,6 +96,7 @@ exports.bitcoinInfo = (req, res) => {
   actionMap.set(ACTION_TOTAL, totalHandler);
   actionMap.set(ACTION_BLOCK, blockCountHandler);
   actionMap.set(ACTION_MARKET, marketCaptHandler);
+  actionMap.set(ACTION_INTERVAL, intervalHandler);
   
   assistant.handleRequest(actionMap);
 };
