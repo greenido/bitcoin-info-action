1a9de3c2fcc42cd64011b79df9c5d5961075fd50
Ido Green <idog@idog-macbookpro.roam.corp.google.com>
2017-06-05 10:21:16 -0700

    Adding the market cap and blocks count

--- index.js
+++ index.js
@@ -23,42 +23,69 @@ const request = require('request');
 // Some variables we will use in this example
 const ACTION_PRICE = 'price';
 const ACTION_TOTAL = 'total';
+const ACTION_BLOCK = 'block';
+const ACTION_MARKET = 'marketcap';
+
 const EXT_BITCOIN_API_URL = "https://blockchain.info";
 const EXT_PRICE = "/q/24hrprice";
 const EXT_TOTAL = "/q/totalbc";
+const EXT_BLOCK_COUNT = "/q/getblockcount";
+const EXT_MARKET_CAP = "/q/marketcap";
 
-// [START YourAction]
+// [START Bitcoin Info]
 exports.bitcoinInfo = (req, res) => {
-    const assistant = new Assistant({ request: req, response: res });
-    console.log('bitcoinInfoAction Request headers: ' + JSON.stringify(req.headers));
-    console.log('bitcoinInfoAction Request body: ' + JSON.stringify(req.body));
+  const assistant = new Assistant({request: req, response: res});
+  console.log('bitcoinInfoAction Request headers: ' + JSON.stringify(req.headers));
+  console.log('bitcoinInfoAction Request body: ' + JSON.stringify(req.body));
 
-    // Fulfill price action business logic
-    function priceHandler(assistant) {
-        request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
-            // The fulfillment logic for returning the bitcoin current price
-            console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-            const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
-            assistant.tell(msg);
-        });
-    }
+  // Fulfill price action business logic
+  function priceHandler (assistant) {
+  	request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
+  		// The fulfillment logic for returning the bitcoin current price
+  		console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+    	const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
+    	assistant.ask(msg);
+  	});
+  }
 
-    // Fulfill total bitcoin action 
-    function totalHandler(assistant) {
-        request(EXT_BITCOIN_API_URL + EXT_TOTAL, function(error, response, body) {
-            console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-            // The fulfillment logic for returning the amount of bitcoins in the world
-            const billionsBitcoins = body / 1000000000;
-            const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
-            assistant.tell(msg);
-        });
-    }
+  // Fulfill total bitcoin action 
+  function totalHandler (assistant) {
+    request(EXT_BITCOIN_API_URL + EXT_TOTAL, function(error, response, body) {
+  		console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+    	// The fulfillment logic for returning the amount of bitcoins in the world
+    	const billionsBitcoins = body / 1000000000;
+    	const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
+    	assistant.ask(msg);
+  	});
+  }
+  
+  // Fulfill block count action
+  function blockCountHandler (assistant) {
+  	request(EXT_BITCOIN_API_URL + EXT_BLOCK_COUNT, function(error, response, body) {
+  		console.log("blockCountHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+    	const msg = "Right now there are " + body + " blocks. What else would you like to know? the price?";
+    	assistant.ask(msg);
+  	});
+  }
 
-    // The Entry point to all our actions
-    const actionMap = new Map();
-    actionMap.set(ACTION_PRICE, priceHandler);
-    actionMap.set(ACTION_TOTAL, totalHandler);
+// Fulfill block count action
+  function marketCaptHandler (assistant) {
+  	request(EXT_BITCOIN_API_URL + EXT_MARKET_CAP, function(error, response, body) {
+  		console.log("marketCaptHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
+  		const marketCapB = Math.round((body / 1000000000) * 100) / 100
+    	const msg = "Right now market cap is " + marketCapB + " billions. What else would you like to know?";
+    	assistant.ask(msg);
+  	});
+  }
 
-    assistant.handleRequest(actionMap);
+
+  // The Entry point to all our actions
+  const actionMap = new Map();
+  actionMap.set(ACTION_PRICE, priceHandler);
+  actionMap.set(ACTION_TOTAL, totalHandler);
+  actionMap.set(ACTION_BLOCK, blockCountHandler);
+  actionMap.set(ACTION_MARKET, marketCaptHandler);
+  
+  assistant.handleRequest(actionMap);
 };
-// [END Action]
+// [END Bitcoin Info]
